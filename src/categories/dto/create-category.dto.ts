3bbdf4f84ece6bdf67fcb7c18a341f4d673f611a
Tharun_Kumar_95 <ext_tharun.kumar@carsome.com>
2022-10-14 09:19:21 +0530

    chore: initial commit

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateCategoryDto {
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty()
